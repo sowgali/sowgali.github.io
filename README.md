68a92c0bede8bc2cbdbbe9aa1d517f7f8abffade
Sowrya Gali <galisowrya@gmail.com>
2020-08-05 17:51:40 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A portfolio website designed to showcase my skills as developer.
---
+++ precache-manifest.bbcb3e485696239092ea55ede7835259.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce5354f1863a01336b1e13e10073df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b850a42e933a6d8ce52",
+    "url": "/static/css/2.5c0b27ce.chunk.css"
+  },
+  {
+    "revision": "355245e1ad7ab60cc7c5",
+    "url": "/static/css/main.cd5b6014.chunk.css"
+  },
+  {
+    "revision": "6b850a42e933a6d8ce52",
+    "url": "/static/js/2.22f4c5bc.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.22f4c5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355245e1ad7ab60cc7c5",
+    "url": "/static/js/main.fba872a2.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
